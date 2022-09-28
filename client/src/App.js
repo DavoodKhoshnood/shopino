2d148bece74fb40b1eff3f44ef6e1fc1393d2a36
davoodkhoshnood <khoshnood1977@gmail.com>
2022-09-28 13:49:40 +0100

    Add products

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 function App() {
   return (
@@ -6,7 +7,23 @@ function App() {
         <a href="/">Shopino</a>
       </header>
       <main>
-        list productds
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => {
+            return (
+              <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                <div className="product-info">
+                  <p>{product.name}</p>
+                  <p><strong>{product.price}</strong></p>
+                  <button>Add to cart</button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </main>
     </div>
   );
