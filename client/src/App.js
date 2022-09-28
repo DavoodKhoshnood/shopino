8d35988951946507a55734e4a48e87a8ca70d675
davoodkhoshnood <khoshnood1977@gmail.com>
2022-09-28 12:22:32 +0100

    Create client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Shopino</a>
+      </header>
+      <main>
+        list productds
+      </main>
+    </div>
+  );
+}
+
+export default App;
