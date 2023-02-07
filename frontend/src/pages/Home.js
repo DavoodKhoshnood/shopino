05d2aeef2a2b5f9cee91e0c99e37c3601615a33d
davoodkhoshnood <khoshnood1977@gmail.com>
2023-02-07 13:52:47 +0000

    Publish to Heroku

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useReducer } from "react";
 import axios from "axios";
-import logger from "use-reducer-logger";
+// import logger from "use-reducer-logger";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from "../components/Product";
