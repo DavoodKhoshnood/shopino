87a7e79382e7415e2d9d4d6203cf84a50060d3f6
davoodkhoshnood <khoshnood1977@gmail.com>
2023-03-09 23:45:06 +0000

    Debug Admin Dashboard

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import Order from "../models/orderModel.js";
+import User from '../models/userModel.js';
+import Product from '../models/productModel.js';
 import { isAuth, isAdmin } from "../utils.js";
 
 const orderRouter = express.Router();
