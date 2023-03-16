657c637de68cf96679b6beee84647ad84e60d065
davoodkhoshnood <khoshnood1977@gmail.com>
2023-03-16 10:42:41 +0000

    Debug Admin Dashboard

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useReducer } from 'react';
+import Chart from 'react-google-charts';
 import { Store } from '../Store';
 import axios from 'axios';
 import { getError } from '../utils';
@@ -7,6 +8,7 @@ import MessageBox from '../components/MessageBox';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
+
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -32,7 +34,7 @@ const Dashboard = () => {
     useEffect(()=> {
         const dataFetch = async () => {
             try {
-                const data = await axios.get('/api/orders/summary', {
+                const { data } = await axios.get('/api/orders/summary', {
                     headers: { Authorization: `Bearer ${userInfo.token}` },
                 });
                 dispatch({
@@ -49,54 +51,92 @@ const Dashboard = () => {
         dataFetch();
     }, [userInfo]);
 
-  return (
-    <div>
-        <h1>Dashboard</h1>
-        {loading 
-            ? ( <LoadingBox /> ) 
-            : ( error ? (
-                <MessageBox variant="danger">{error}</MessageBox>
-            ): (
-                <>
-                    <Row>
-                    <Col md={4}>
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>
-                                        {summary.users && summary.users[0] ? summary.users[0].numUsers : 0 }
-                                    </Card.Title>
-                                    <Card.Text>Users</Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col md={4}>
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>
-                                        {summary.orders && summary.users[0] ? summary.orders[0].numOrders : 0 }
-                                    </Card.Title>
-                                    <Card.Text>Orders</Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col md={4}>
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>
-                                        {summary.orders && summary.users[0] ? summary.orders[0].totalSales.toFixed(2) : 0 }
-                                    </Card.Title>
-                                    <Card.Text>Users</Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-
-                    </Row>
-                </>
-                )
-            )
-        }
-    </div>
-  )
-}
+    return (
+        <div>
+          <h1>Dashboard</h1>
+          {loading ? (
+            <LoadingBox />
+          ) : error ? (
+            <MessageBox variant="danger">{error}</MessageBox>
+          ) : (
+            <>
+              <Row>
+                <Col md={4}>
+                  <Card>
+                    <Card.Body>
+                      <Card.Title>
+                        {summary.users && summary.users[0]
+                          ? summary.users[0].numUsers
+                          : 0}
+                      </Card.Title>
+                      <Card.Text> Users</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col md={4}>
+                  <Card>
+                    <Card.Body>
+                      <Card.Title>
+                        {summary.orders && summary.users[0]
+                          ? summary.orders[0].numOrders
+                          : 0}
+                      </Card.Title>
+                      <Card.Text> Orders</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col md={4}>
+                  <Card>
+                    <Card.Body>
+                      <Card.Title>
+                        $
+                        {summary.orders && summary.users[0]
+                          ? summary.orders[0].totalSales.toFixed(2)
+                          : 0}
+                      </Card.Title>
+                      <Card.Text> Orders</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+              <div className="my-3">
+                <h2>Sales</h2>
+                {summary.dailyOrders.length === 0 ? (
+                  <MessageBox>No Sale</MessageBox>
+                ) : (
+                  <Chart
+                    width="100%"
+                    height="400px"
+                    chartType="AreaChart"
+                    loader={<div>Loading Chart...</div>}
+                    data={[
+                      ['Date', 'Sales'],
+                      ...summary.dailyOrders.map((x) => [x._id, x.sales]),
+                    ]}
+                  ></Chart>
+                )}
+              </div>
+              <div className="my-3">
+                <h2>Categories</h2>
+                {summary.productCategories.length === 0 ? (
+                  <MessageBox>No Category</MessageBox>
+                ) : (
+                  <Chart
+                    width="100%"
+                    height="400px"
+                    chartType="PieChart"
+                    loader={<div>Loading Chart...</div>}
+                    data={[
+                      ['Category', 'Products'],
+                      ...summary.productCategories.map((x) => [x._id, x.count]),
+                    ]}
+                  ></Chart>
+                )}
+              </div>
+            </>
+          )}
+        </div>
+      );
+    }
 
 export default Dashboard
