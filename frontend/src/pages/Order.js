a312168fb3f249254b005cd1cdf87bcb8dd0dec6
davoodkhoshnood <khoshnood1977@gmail.com>
2023-03-23 12:13:28 +0000

    Admin Orders - Deliver Order

--- frontend/src/pages/Order.js
+++ frontend/src/pages/Order.js
@@ -15,6 +15,7 @@ import MessageBox from "../components/MessageBox";
 import { Store } from "../Store";
 import { getError } from "../utils";
 import { toast } from "react-toastify";
+import Button from "react-bootstrap/Button";
 
 function reducer(state, action) {
   switch (action.type) {
@@ -32,6 +33,18 @@ function reducer(state, action) {
       return { ...state, loadingPay: false };
     case "PAY_RESET":
       return { ...state, loadingPay: false, successPay: false };
+    case 'DELIVER_REQUEST':
+      return { ...state, loadingDeliver: true };
+    case 'DELIVER_SUCCESS':
+      return { ...state, loadingDeliver: false, successDeliver: true };
+    case 'DELIVER_FAIL':
+      return { ...state, loadingDeliver: false };
+    case 'DELIVER_RESET':
+      return {
+        ...state,
+        loadingDeliver: false,
+        successDeliver: false,
+      };
     default:
       return state;
   }
@@ -46,7 +59,7 @@ export default function Order() {
 
   // const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
   const navigate = useNavigate();
-  const [{ loading, error, order, successPay, loadingPay }, dispatch] =
+  const [{ loading, error, order, successPay, loadingPay, loadingDeliver, successDeliver, }, dispatch] =
     useReducer(reducer, {
       loading: true,
       error: "",
@@ -106,10 +119,13 @@ export default function Order() {
     if (!userInfo) {
       return navigate("/login");
     }
-    if (!order._id || successPay || (order._id && order._id !== orderId)) {
+    if (!order._id || successPay || successDeliver || (order._id && order._id !== orderId)) {
       fetchOrder();
       if (successPay) {
         dispatch({ type: "PAY_RESET" });
+      }
+      if (successDeliver) {
+        dispatch({ type: 'DELIVER_RESET' });
       }
     } else {
       const loadPayPalScript = async () => {
@@ -127,7 +143,25 @@ export default function Order() {
       };
       loadPayPalScript();
     }
-  }, [order, userInfo, orderId, paypalDispatch, successPay, navigate]);
+  }, [order, userInfo, orderId, paypalDispatch, successDeliver, successPay, navigate]);
+
+  async function deliverOrderHandler() {
+    try {
+      dispatch({ type: 'DELIVER_REQUEST' });
+      const { data } = await axios.put(
+        `/api/orders/${order._id}/deliver`,
+        {},
+        {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      dispatch({ type: 'DELIVER_SUCCESS', payload: data });
+      toast.success('Order is delivered');
+    } catch (err) {
+      toast.error(getError(err));
+      dispatch({ type: 'DELIVER_FAIL' });
+    }
+  }
 
   return loading ? (
     <LoadingBox></LoadingBox>
@@ -252,7 +286,9 @@ export default function Order() {
                 {userInfo.isAdmin && order.isPaid && !order.isDelivered && (
                   <ListGroup.Item>
                     <div className="d-grid">
-            
+                      <Button type="button" onClick={deliverOrderHandler}>
+                        Deliver Order
+                      </Button>            
                     </div>
                   </ListGroup.Item>
                 )}
--- frontend/src/pages/OrdersList.js
+++ frontend/src/pages/OrdersList.js
@@ -82,7 +82,6 @@ const OrdersList = () => {
                 <td>{order.createdAt.substring(0, 10)}</td>
                 <td>{order.totalPrice.toFixed(2)}</td>
                 <td>{order.isPaid ? order.paidAt.substring(0, 10) : 'No'}</td>
-                <td>{order.isPaid ? order.paidAt.substring(0, 10) : 'No'}</td>
                 <td>
                   {order.isDelivered
                     ? order.deliveredAt.substring(0, 10)
